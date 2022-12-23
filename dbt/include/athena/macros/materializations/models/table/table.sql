10f80036618185ffc3f5fac2f9eed31246a513bc
Ojasvi Harsola <ojasvi@blotout.io>
2022-12-23 20:11:02 +0530

    add table_zero_downtime tag

--- dbt/include/athena/macros/materializations/models/table/table.sql
+++ dbt/include/athena/macros/materializations/models/table/table.sql
@@ -11,18 +11,37 @@
   {{ run_hooks(pre_hooks) }}
 
   -- cleanup
-  {%- if old_relation is not none -%}
-    {{ drop_relation(old_relation) }}
+  {% if not var('table_zero_downtime', false) and 'table_zero_downtime' not in config.get('tags') %}
+    {%- if old_relation is not none -%}
+      {{ drop_relation(old_relation) }}
+    {%- endif -%}
   {%- endif -%}
 
   -- build model
-  {% call statement('main') -%}
-    {{ create_table_as(False, target_relation, sql) }}
-  {%- endcall %}
+  {% if var('table_zero_downtime', false) or 'table_zero_downtime' in config.get('tags') %}
+    {%- set current_ts = (modules.datetime.datetime.utcnow() - modules.datetime.datetime.utcfromtimestamp(0)).total_seconds() * 1000 -%}
+    {%- set ctas_id_str = "{0}_{1}".format(identifier, current_ts) -%}
+    {%- set ctas_id = ctas_id_str[0:ctas_id_str.index('.')] -%}
+    {%- set ctas_relation = '"{0}"."{1}"."{2}"'.format(database, schema, ctas_id)  -%}
+    {% call statement('main') -%}
+      {{ create_table_as(False, ctas_relation, sql) }}
+    {% endcall -%}
+    {% call statement('main') -%}
+        {%- set view_relation = api.Relation.create(identifier='view_'~identifier,
+                                                        schema=schema,
+                                                        database=database,
+                                                        type='table') -%}
+        {{ create_view_as(view_relation, "SELECT * FROM " ~ ctas_relation) }}
+    {% endcall -%}
+  {%- else -%}
+    {% call statement('main') -%}
+      {{ create_table_as(False, target_relation, sql) }}
+    {%- endcall %}
 
-  {% if table_type != 'iceberg' %}
-    {{ set_table_classification(target_relation) }}
-  {% endif %}
+    {% if table_type != 'iceberg' %}
+      {{ set_table_classification(target_relation) }}
+    {% endif %}
+  {%- endif -%}
 
   {{ run_hooks(post_hooks) }}
 
